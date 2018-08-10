9b2c57495c1c4c0de91d7eff6059abe3d8cfe3e8
Lucas Merciyah <lucasmerciyah@Lucass-MacBook-Pro.local>
2018-08-10 19:59:31 +0100

    All wired up with Firebase

--- app/ui/home.js
+++ app/ui/home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, ImageBackground, Image, TouchableOpacity } from 'react-native';
+import {Navigator} from 'react-native-deprecated-custom-components'
+
 
 // This is the default page on new Merciyaj instance.
 
@@ -8,7 +10,12 @@ export default class Home extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Hello World!</Text>
-        <Text>This is our Home View</Text>
+                <Navigator
+              style={{flex: 1}}
+              ref={'NAV'}
+              initialRoute={{id: 'home', name: 'home'}}
+              renderScene={this.renderScene.bind(this)}
+            /> 
       </View>
     );
   }
--- app/ui/index.js
+++ app/ui/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 // This is were all the mathematics and design sheeets get referenced.
 // Dont get confused
@@ -24,6 +24,13 @@ const firebaseConfig = {
   databaseURL: fireConfig.databaseURL,
   storageBucket: fireConfig.storageBucket,
 };
+
+//sample data
+var anint = 300
+var array = [1,2,2,3]
+var string = "That Brittany is on my case again"
+
+
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 const routes = {
@@ -33,22 +40,35 @@ const routes = {
   home: Home
 };
 
+// Init Database 
+var dataRef = firebaseApp.database().ref('dataTypes/');
+
 export default class index extends React.Component {
+  constructor(props){
+    super(props)
+
+    console.log(firebaseApp);
+  }
   // routing to our views dynamicly
   renderScene({id}, navigator){
     const Scene = routes[id]
     return <Scene {...this.props} hideMenu={() => this.hideMenu()} pushToCache = {this.pushToCash} navigator={navigator}/>
   }
 
+  writeData(str, int, arr) {
+  dataRef.set({
+    string:string,
+   array : array,
+   numb: anint
+  });
+}
+
   render() {
     return (
       <View style={styles.container}>
-        <Navigator
-              style={{flex: 1}}
-              ref={'NAV'}
-              initialRoute={{id: 'home', name: 'home'}}
-              renderScene={this.renderScene.bind(this)}
-            /> 
+      <TouchableOpacity onPress = {() => {this.writeData()}} style={{padding:5,width:70, height:70, borderRadius:10, backgroundColor:'#A58792', alignItems:'center', justifyContent:'center'}}>
+      <Text style={{color:'#fff'}}>Write</Text>
+      </TouchableOpacity>
       </View>
     );
   }
@@ -57,6 +77,8 @@ export default class index extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems:'center',
+    justifyContent:'center',
     backgroundColor: '#fff',
   },
 });
